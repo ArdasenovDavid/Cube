20353b0fd4352dd75fe332dbf5a139e26fe8742f
ArdasenovDavid <68203426+ArdasenovDavid@users.noreply.github.com>
2022-08-18 22:04:05 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector('.btn');
+const cube = document.querySelector('.cube');
+btn.addEventListener('click', () => {
+	cube.style.animation = "anime 4s linear infinite"
+})
